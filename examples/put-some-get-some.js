72ed03565adf5d63ff262a6f1d755e28eac01bf0
Paul Vorbach <paul@vorb.de>
2013-03-05 10:39:21 +0100

    Add example

---
+++ examples/put-some-get-some.js
@@ -0,0 +1,32 @@
+var mem = require('../');
+var async = require('async');
+
+async.waterfall([ function(callback) {
+  mem.connect({
+    name : 'persistence_test'
+  }, callback)
+}, function(db, callback) {
+  db.getCollection('author', [ '_id' ], callback);
+}, function(collection, callback) {
+  collection.save({
+    name : 'pvorb',
+  }, function(err, saved) {
+    if (err)
+      return callback(err);
+
+    console.log('saved', saved);
+
+    callback(null, collection);
+  });
+}, function(collection, callback) {
+  collection.find({
+    name : 'pvorb'
+  }, callback);
+} ], function(err, records) {
+  if (err)
+    throw err;
+
+  records.forEach(function(record) {
+    console.log('found', record)
+  });
+});
